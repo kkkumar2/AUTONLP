4153d19584e3cbfd94cc66c681fa1b4c7c82ca2e
sandeep <jena.sandp@gmail.com>
2022-07-27 17:15:27 +0530

    requirements and setup.py updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import os
+
+setup(
+    name="MAIN",
+    version="0.0.1",
+    packages=["MAIN"],
+    python_requires=">=3.6",
+    # install_requires=[
+
+    # ]
+    )
